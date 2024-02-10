065a9b094840ea1e129907d6820564b0bb1784ac
aishaa020 <144467028+aishaa020@users.noreply.github.com>
2024-02-10 23:41:46 +0200

    commit 4

---
+++ app/src/main/java/API/FirebaseAPI.java
@@ -0,0 +1,227 @@
+package API;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.project.GitStartedActivity;
+import com.example.project.MainActivity;
+import com.example.project.R;
+import com.example.project.UserProfileActivity;
+import com.example.project.loginActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import datamodel.User;
+
+public class FirebaseAPI {
+    static String URL = "http://10.0.2.2:5000/";
+    private RequestQueue queue;
+
+    public  FirebaseAPI(Context context) {
+        queue = Volley.newRequestQueue(context);
+
+    }
+
+    public void register(User user,Context context) throws JSONException {
+        String url = URL + "user";
+
+
+        try {
+
+            // Make new json object and put params in it
+            JSONObject jsonParams = new JSONObject();
+            jsonParams.put("name", user.getName());
+            jsonParams.put("location", user.getLocation());
+            jsonParams.put("birthday", user.getBirthday());
+            jsonParams.put("email", user.getEmail());
+            jsonParams.put("password", user.getPassword());
+
+
+            // Building a request
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,  URL+"user",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            Toast.makeText(context, "successful", Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                            Toast.makeText(context, "error occurred", Toast.LENGTH_SHORT).show();
+                            error.printStackTrace();
+                        }
+                    }){
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    return jsonParams.toString().getBytes(StandardCharsets.UTF_8);
+                }
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+            };
+
+
+             queue.add(stringRequest);
+
+        } catch (JSONException ex) {
+            throw ex;
+        }
+
+
+
+    }
+
+    public void login(String email,String password,Context context) throws JSONException {
+        String url = URL + "user";
+
+
+        try {
+
+            // Make new json object and put params in it
+            JSONObject jsonParams = new JSONObject();
+            jsonParams.put("email", email);
+            jsonParams.put("password", password);
+
+
+            // Building a request
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,  URL+"login",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if(response.equals("false")) {
+                                Toast.makeText(context, "wrong confidentials", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(context, "successful", Toast.LENGTH_SHORT).show();
+                                //change the login to your landing page once you uploaded it to githup
+                                Intent intent = new Intent(context, UserProfileActivity.class);
+                                intent.putExtra("id",response);
+                                startActivity(context, intent, null);
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                            Toast.makeText(context, "error occurred", Toast.LENGTH_SHORT).show();
+                            error.printStackTrace();
+                        }
+                    }){
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    return jsonParams.toString().getBytes(StandardCharsets.UTF_8);
+                }
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+            };
+
+
+            queue.add(stringRequest);
+
+        } catch (JSONException ex) {
+            throw ex;
+        }
+
+
+
+    }
+
+    public void get_user_data(String id,Context context) throws JSONException {
+        String url = URL + "profile";
+
+
+        try {
+
+            JSONObject jsonParams = new JSONObject();
+            jsonParams.put("id", id);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,  URL+"profile",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            ImageView imageView = (ImageView) ((Activity) context).findViewById(R.id.imageView);
+                            TextView nameTextView = (TextView) ((Activity) context).findViewById(R.id.name);
+                            TextView dateTextView = (TextView) ((Activity) context).findViewById(R.id.date);
+                            TextView locationTextView = (TextView) ((Activity) context).findViewById(R.id.location);
+                            TextView emailTextView = (TextView) ((Activity) context).findViewById(R.id.email);
+                            Button selectImageButton = (Button) ((Activity) context).findViewById(R.id.selectImageButton);
+
+                            try {
+                                JSONObject obj = new JSONObject(response);
+                                String name = obj.getString("name");
+                                String date = obj.getString("birthday");
+                                String location = obj.getString("location");
+                                String email = obj.getString("email");
+                                nameTextView.setText(name);
+                                dateTextView.setText(date);
+                                locationTextView.setText(location);
+                                emailTextView.setText(email);
+                            } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                            }
+
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                            Toast.makeText(context, "error occurred", Toast.LENGTH_SHORT).show();
+                            error.printStackTrace();
+                        }
+                    }){
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    return jsonParams.toString().getBytes(StandardCharsets.UTF_8);
+                }
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+            };
+
+
+            queue.add(stringRequest);
+
+        } catch (JSONException ex) {
+            throw ex;
+        }
+
+
+
+    }
+
+}
+
--- app/src/main/java/com/example/project/Adabters/itemAdapter.java
+++ app/src/main/java/com/example/project/Adabters/itemAdapter.java
@@ -4,17 +4,22 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.project.Items_holder;
+import com.airbnb.lottie.LottieAnimationView;
+
 import com.example.project.R;
+import com.example.project.databinding.ItemRowBinding;
 import com.example.project.models.ItemsModel;
 
 import java.util.List;
 
-public class itemAdapter extends RecyclerView.Adapter<Items_holder> {
+public class itemAdapter extends RecyclerView.Adapter<itemAdapter.Items_holder> {
 
     Context context;
     List<ItemsModel> items;
@@ -33,22 +38,22 @@ public class itemAdapter extends RecyclerView.Adapter<Items_holder> {
 
     @Override
     public void onBindViewHolder(@NonNull Items_holder holder, int position) {
-        holder.getItemimg1().setImageResource(items.get(position).getImg());
-        holder.getTxtprice1().setText(items.get(position).getPrice());
-//        holder.getBtn_atc1().setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //intent
-//
-//            }
-//        });
-//        holder.getLikelottie1().setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                holder.getLikelottie1().playAnimation();
-//                //toast
-//            }
-//        });
+        holder.binding.itemimg1.setImageResource(items.get(position).getImg());
+        holder.binding.txtprice1.setText(items.get(position).getPrice());
+        holder.binding.btnAtc1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //intent
+
+            }
+        });
+        holder.binding.likelottie1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //holder.getLikelottie1().playAnimation();
+                //toast
+            }
+        });
 
 //        holder.getItemimg1().setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -64,6 +69,65 @@ public class itemAdapter extends RecyclerView.Adapter<Items_holder> {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
+
+    public static class Items_holder extends RecyclerView.ViewHolder{
+
+        ImageView itemimg1;
+        TextView txtprice1;
+
+        Button btn_atc1;
+
+        LottieAnimationView likelottie1;
+
+
+        ItemRowBinding binding;
+        public Items_holder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemRowBinding.bind(itemView);
+            itemimg1 = itemView.findViewById(R.id.itemimg1);
+            txtprice1 = itemView.findViewById(R.id.txtprice1);
+            btn_atc1 = itemView.findViewById(R.id.btn_atc1);
+            likelottie1 = itemView.findViewById(R.id.likelottie1);
+
+
+        }
+
+        public ImageView getItemimg1() {
+            return itemimg1;
+        }
+
+        public void setItemimg1(ImageView itemimg1) {
+            this.itemimg1 = itemimg1;
+        }
+
+
+
+        public TextView getTxtprice1() {
+            return txtprice1;
+        }
+
+        public void setTxtprice1(TextView txtprice1) {
+            this.txtprice1 = txtprice1;
+        }
+
+
+        public Button getBtn_atc1() {
+            return btn_atc1;
+        }
+
+        public void setBtn_atc1(Button btn_atc1) {
+            this.btn_atc1 = btn_atc1;
+        }
+
+        public LottieAnimationView getLikelottie1() {
+            return likelottie1;
+        }
+
+        public void setLikelottie1(LottieAnimationView likelottie1) {
+            this.likelottie1 = likelottie1;
+        }
+    }
+
 }
